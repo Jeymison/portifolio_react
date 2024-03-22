c629d110fadd0b320f5c86af07b1792abc5d008d
Jeymison Menezes <jeymisong@gmail.com>
2024-03-22 18:05:05 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from "./styles";
+
+const Avatar = () => <FotoPerfil src="https://github.com/Jeymison.png" />;
+
+export default Avatar;
--- src/containers/SideBar/index.tsx
+++ src/containers/SideBar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from "../../components/Avatar";
+import Paragrafo from "../../components/Paragrafo";
 import Titulo from "../../components/Titulo";
+import { Descricao, ButtonTema, SideBarContainer } from "./styles";
 
 const SideBar = () => (
   <aside>
-    <img src="https://github.com/Jeymison.png" />
-    <Titulo fontSize={20}>Jeymison Menezes</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Jeymison Menezes</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        Jeymison
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor
+      </Descricao>
+      <ButtonTema>Trocar tema</ButtonTema>
+    </SideBarContainer>
   </aside>
 );
 
